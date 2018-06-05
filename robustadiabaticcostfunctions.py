d7c84809b6a7fc4bf41b0f941fd56153b636d2e1
Anupam Mitra <anupam.mitra@gmail.com>
2018-06-05 12:09:41 -0600

    Do propagator and propagator gradient calculations simultaneously.

--- robustadiabaticcostfunctions.py
+++ robustadiabaticcostfunctions.py
@@ -87,6 +87,7 @@ def infidelity_unitary(phi, control_params):
         control_params['Nstates'] = Nstates
         
     F_average = 0
+    F_average_gradient = np.zeros(Nsteps)
     for deltaRa, deltaRb in itertools.product(deltaR_values, deltaR_values):
         
         control_params['PropagatorParameters']['HamiltonianParameters']['DeltaRa'] \
@@ -94,37 +95,17 @@ def infidelity_unitary(phi, control_params):
         control_params['PropagatorParameters']['HamiltonianParameters']['DeltaRb'] \
         = DeltaR + deltaRb
       
-        u = propagators.propagator(phi, u_params)
+        u, u_gradient  = propagators.propagator(phi, u_params)
         udagger = u.transpose().conjugate()
     
         u_dress = u_dress_dict.get((deltaRa, deltaRb))
         u_undress = u_dress_dict.get((deltaRa, deltaRb))
     
         u_protocol = np.dot(u_dress, np.dot(u, u_undress))
+        u_protocol_dagger = np.conjugate(np.transpose(u_protocol))
         
         F = np.abs(np.trace(np.dot(u_protocol, u_target_dagger)))**2 / Nstates**2
         F_average += F 
-    
-    F_average = F_average/Nterms_average
-    I_average = 1 - F_average
-
-    F_average_gradient = np.zeros(Nsteps)
-
-    for deltaRa, deltaRb in itertools.product(deltaR_values, deltaR_values):
-        
-        control_params['PropagatorParameters']['HamiltonianParameters']['DeltaRa'] \
-        = DeltaR + deltaRa
-        control_params['PropagatorParameters']['HamiltonianParameters']['DeltaRb'] \
-        = DeltaR + deltaRb
-        
-        u = propagators.propagator(phi, u_params)
-        u_gradient = propagators.propagator_gradient(phi, u_params)
-        
-        u_dress = u_dress_dict.get((deltaRa, deltaRb))
-        u_undress = u_undress_dict.get((deltaRa, deltaRb))
- 
-        u_protocol = np.dot(u_undress, np.dot(u, u_dress))
-        u_protocol_dagger = np.conjugate(np.transpose(u_protocol))
         
         for n in range(Nsteps):
             u_protocol_gradient_step = np.dot(u_undress, np.dot(u_gradient[n], u_dress))
@@ -133,59 +114,13 @@ def infidelity_unitary(phi, control_params):
               np.trace(np.dot(u_target_dagger, u_protocol_gradient_step))
               * np.trace(np.dot(u_target, u_protocol_dagger))) / Nstates**2
 
+
+    F_average = F_average/Nterms_average
+    I_average = 1 - F_average
+
     F_average_gradient /= Nterms_average        
     I_average_gradient = - F_average_gradient
 
 
     return I_average, I_average_gradient
 
-def infidelity_unitary_gradient (phi, control_params):
-    """
-    Computes the infidelity for a control task 
-    for given values of the control variables
-    for inhomogeneities
-    
-    As of now it is very specific to Rydberg atoms
-    
-    Parameters
-    ----------
-    phi:
-    Values of the control variables at which to evaluate the
-    gradient of the propagator
-    
-    control_params:
-    Dictionary representing parameters of the control problem
-    with the following keys
-    
-    Returns
-    -------
-   
-    """
-
-    h_inhomo = control_params['HamiltonianUncertainParameters']
-    
-    deltaR_values = h_inhomo['deltaDeltaRValues']
-    
-    Nterms_average = len(deltaR_values)**2
-
-    
-    DeltaR = control_params['HamiltonianBaseParameters']['DeltaR']
-
-    u_params = control_params.get('PropagatorParameters')
-    
-    u_target = control_params.get('UnitaryTarget')
-    Nstates = control_params.get('NStatesUnitary')
-    
-    u_dress_dict = control_params.get('UnitaryDressing')
-    u_undress_dict = control_params.get('UnitaryUndressing')
-    
-    u_target_dagger = u_target.conjugate().transpose()
-    
-    # Check for the matrix rank of the desired unitary to use for calculating
-    # infidelity for a partial isometry
-    if Nstates == None:
-        Nstates = np.linalg.matrix_rank(u_target)
-        control_params['Nstates'] = Nstates
-        
-    
-    return I_average_gradient
